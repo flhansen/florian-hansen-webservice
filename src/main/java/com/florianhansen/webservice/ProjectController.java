0b02c14b62de737026950cfe21aa96c3c976e468
Florian Hansen <flhansen.93@gmail.com>
2021-12-22 12:06:12 +0100

    Implement first api route to fetch project descriptions.

--- src/main/java/com/florianhansen/webservice/ProjectController.java
+++ src/main/java/com/florianhansen/webservice/ProjectController.java
@@ -1,14 +1,34 @@
 package com.florianhansen.webservice;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.florianhansen.webservice.data.Project;
+import com.florianhansen.webservice.data.ProjectRepository;
+
 @RestController
 public class ProjectController {
+	
+	@Autowired
+	private ProjectRepository projectRepo;
 
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/projects")
+	public String projects() {
+		List<Project> projects = projectRepo.findAll();
+		List<String> projectNames = projects.stream()
+											.map(p -> p.getDescription())
+											.collect(Collectors.toList());
+
+		return String.join(", ", projectNames);
 	}
 	
 }
---
+++ src/main/java/com/florianhansen/webservice/data/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.florianhansen.webservice.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+}
